892aa458f65bfbf6fa31ef311a5326ce7777de06
shame-game <142651893+shame-game@users.noreply.github.com>
2024-05-15 14:38:19 +0700

    d

--- js/index.js
+++ js/index.js
@@ -3,13 +3,40 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+let w = screen.width
+console.log(w);
+if (w > 992) {
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+        document.querySelector('.sign-in').setAttribute('style', 'display:none')
+    });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+        document.querySelector('.sign-in').setAttribute('style', 'display:unset')
+    });
+} else {
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+        document.querySelector('.sign-in').setAttribute('style', 'display:none')
+        document.querySelector('.toggle').setAttribute('style', 'left: -125%;')
+    });
+
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+        document.querySelector('.sign-in').setAttribute('style', 'display:unset')
+        document.querySelector('.toggle').setAttribute('style', 'left: -150%;')
+    });
+}
+
+fetchSheet
+    .fetch({
+        gSheetId: '1w0ZWTWCwLovFMRhTHiYAS9yst4qptwjLRcI2GncXUjI',
+        wSheetName: 'header',
+    })
+    .then((rows) => {
+        document.querySelector('#header_logo').src = rows[0]['logo']
+    });
 
 fetchSheet
     .fetch({
@@ -42,6 +69,7 @@ fetchSheet
         wSheetName: 'DoiTuongThamGia',
     })
     .then((rows) => {
+        document.querySelector('.vam_titleup1').innerText = rows[0]['Tiêu đề']
         document.querySelector('#object_title').innerText = rows[0]['Tiêu đề']
         document.querySelector('#object_title1').innerText = rows[0]['Tiêu đề bảng']
         document.querySelector('#object_content1').innerText = rows[0]['Nội dung bảng']
@@ -49,6 +77,9 @@ fetchSheet
         document.querySelector('#object_content2').innerText = rows[1]['Nội dung bảng']
         document.querySelector('#object_title3').innerText = rows[2]['Tiêu đề bảng']
         document.querySelector('#object_content3').innerText = rows[2]['Nội dung bảng']
+        document.querySelectorAll('.object_wrapimg>img')[0].src = rows[0]['Hình ảnh']
+        document.querySelectorAll('.object_wrapimg>img')[1].src = rows[1]['Hình ảnh']
+        document.querySelectorAll('.object_wrapimg>img')[2].src = rows[2]['Hình ảnh']
     });
 
 
@@ -60,35 +91,57 @@ fetchSheet
     .then((rows) => {
         let d = ""
         let i = 1
+        let f = ''
+        document.querySelector('#timeline_titleup').innerText = rows[0]['Tiêu đề']
         document.querySelector('#timeline_title').innerText = rows[0]['Tiêu đề']
         rows.forEach((t) => {
-            if (i % 2 === 0) {
-                d +=
-                    `<div class="timeline_items">
-                    <div class="timeline_item-main l">
-                        <h2>${t.time}</h2>
-                        <div class="timeline_item-content">
-                            <p>${t.content}</p>
-                        </div>
-                        <span class="timeline_item-index l">${i}</span>
-                    </div>
-                </div>`
-            } else {
-                d +=
-                    `<div class="timeline_items">
-                    <div class="timeline_item-main c">
-                        <span class="timeline_item-index c">${i}</span>
 
-                        <div class="timeline_item-content">
-                            <p>${t.content}</p>
+            if (w < 640) {
+                f +=
+                    `<div class="timeline_items">
+                        <div class="timeline_item-main l">
+                            <h2>${t.time}</h2>
+                            <div class="timeline_item-content">
+                                <p>${t.content}</p>
+                            </div>
+                            <span class="timeline_item-index l">${i}</span>
                         </div>
-                        <h2>${t.time}</h2>
-                    </div>
-                </div>`
+                    </div>`
+                i++
+            } else {
+                if (i % 2 === 0) {
+                    d +=
+                        `<div class="timeline_items">
+                        <div class="timeline_item-main l">
+                            <h2>${t.time}</h2>
+                            <div class="timeline_item-content">
+                                <p>${t.content}</p>
+                            </div>
+                            <span class="timeline_item-index l">${i}</span>
+                        </div>
+                    </div>`
+                } else {
+                    d +=
+                        `<div class="timeline_items">
+                        <div class="timeline_item-main c">
+                            <span class="timeline_item-index c">${i}</span>
+    
+                            <div class="timeline_item-content">
+                                <p>${t.content}</p>
+                            </div>
+                            <h2>${t.time}</h2>
+                        </div>
+                    </div>`
+                }
+                i++
             }
-            i++
         })
-        document.querySelector('#timeline_list-wrap').innerHTML = d
+        if (w < 640) {
+            document.querySelector('#timeline_list-wrap').innerHTML = f
+        } else {
+            document.querySelector('#timeline_list-wrap').innerHTML = d
+        }
+
     });
 
 jQuery(document).ready(function ($) {
@@ -143,14 +196,45 @@ fetchSheet
     })
     .then((rows) => {
         let r = ''
+        let i = 0
         rows.forEach((t) => {
             r +=
-                `<div class="question_div">
+                `<div class="question_div" index="${i}">
                     <p class="vam_content">${t['Câu hỏi']}</p>
-                    <i class="bi bi-plus-circle"></i>
+                    <i class="bi bi-arrow-up-circle-fill"></i>
+                </div>
+                <div class="question_load" index="${i}">
+                    <p class="vam_content">${t['Câu trả lời']}</p>
                 </div>`
+            i++
         })
         document.querySelector('#question_load').innerHTML += r
+        document.querySelectorAll('.question_div').forEach((c) => {
+            c.onclick = () => {
+                let i = c.getAttribute('index')
+                document.querySelector(`.question_load[index="${i}"]`).classList.toggle('hide')
+                document.querySelector(`.question_div[index="${i}"]>i`).classList.toggle('hide')
+            }
+        })
+    });
+fetchSheet
+    .fetch({
+        gSheetId: '1w0ZWTWCwLovFMRhTHiYAS9yst4qptwjLRcI2GncXUjI',
+        wSheetName: 'Giaithuong',
+    })
+    .then((rows) => {
+        document.querySelector('#prize_titleup').innerHTML = rows[0]['Tiêu đề']
+        document.querySelector('#prize_title').innerHTML = rows[0]['Tiêu đề']
+        rows.forEach((t) => {
+            if (t['Vòng sơ loại'] != '') {
+
+            }
+        })
+        rows.forEach((t) => {
+            if (t['Vòng chung kết'] != '') {
+
+            }
+        })
     });
 
 fetchSheet
@@ -159,12 +243,14 @@ fetchSheet
         wSheetName: 'HinhThucToChuc',
     })
     .then((rows) => {
+        document.querySelector('#organizational_titleup').innerText = rows[0]['Tiêu đề']
         document.querySelector('#organizational_title').innerText = rows[0]['Tiêu đề']
         document.querySelector('#organizational_titlep').innerText = rows[0]['Tiêu đề phụ']
         let r = ''
         rows.forEach((t) => {
-            r += `<p class="vam_content">${t['Nội dung']}</p>`
+            r += `<p style="text-indent: 10%;" class="vam_content">${t['Nội dung']}</p>`
         })
+        document.querySelector('#organizational_img1>img').src = rows[0]['Hình ảnh']
         document.querySelector('#organizational_content1').innerHTML += r
     });
 
@@ -180,30 +266,56 @@ fetchSheet
         let u = ''
         let i = ''
         rows.forEach((tab) => {
-            r +=
-                `<div style="width:16%;display:flex;justify-content: center;align-items: center;">
+            if (tab['Hình ảnh logo đơn vị chỉ đạo'] != '') {
+                r +=
+                    `<div class="donvi_load-wrap">
                     <img style="width:80%" src="${tab['Hình ảnh logo đơn vị chỉ đạo']}"/>
                 </div>`
-            t +=
-                `<div style="width:16%;display:flex;justify-content: center;align-items: center;">
+            }
+            if (tab['Hình ảnh logo đơn vị đồng hành'] != '') {
+                t +=
+                    `<div class="donvi_load-wrap">
                     <img style="width:80%" src="${tab['Hình ảnh logo đơn vị đồng hành']}"/>
                 </div>`
-            y +=
-                `<div style="width:16%;display:flex;justify-content: center;align-items: center;">
+            }
+            if (tab['Hình ảnh logo đơn vị đồng tổ chức'] != '') {
+                y +=
+                    `<div class="donvi_load-wrap">
                     <img style="width:80%" src="${tab['Hình ảnh logo đơn vị đồng tổ chức']}"/>
                 </div>`
-            u +=
-                `<div style="width:16%;display:flex;justify-content: center;align-items: center;">
+            }
+            if (tab['Hình ảnh logo đơn vị truyền thông'] != '') {
+                u +=
+                    `<div class="donvi_load-wrap">
                     <img style="width:80%" src="${tab['Hình ảnh logo đơn vị truyền thông']}"/>
                 </div>`
-            i +=
-                `<div style="width:16%;display:flex;justify-content: center;align-items: center;">
+            }
+            if (tab['Hình ảnh logo đơn vị tổ chức'] != '') {
+                i +=
+                    `<div class="donvi_load-wrap">
                     <img style="width:80%" src="${tab['Hình ảnh logo đơn vị tổ chức']}"/>
                 </div>`
+            }
+
         })
         document.querySelector('#chidao_load').innerHTML += r
         document.querySelector('#donghanh_load').innerHTML += t
         document.querySelector('#tochuc_load').innerHTML += y
         document.querySelector('#truyenthong_load').innerHTML += u
         document.querySelector('#tochucc_load').innerHTML += i
+    });
+
+
+fetchSheet
+    .fetch({
+        gSheetId: '1w0ZWTWCwLovFMRhTHiYAS9yst4qptwjLRcI2GncXUjI',
+        wSheetName: 'intro',
+    })
+    .then((rows) => {
+        document.querySelector('#load_about').innerHTml += rows[0]['Về chúng tôi']
+        document.querySelector('#intro_title').innerText = rows[0]['Tiêu đề']
+        document.querySelector('#intro_background').src = rows[0]['background']
+        document.querySelector('#intro_link').src = rows[0]['Link tham gia']
+        document.querySelector('#intro_img').src = rows[0]['img']
+        document.querySelector('#intro_logo').src = rows[0]['logo']
     });
